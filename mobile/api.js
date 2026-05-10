de28b9355d41b4760d918e5e7f97168af375a55b
Ali Murtaza <alimurtaza.9dev@gmail.com>
2026-05-10 16:45:50 +0500

    🔧 Fix static IP in api.js

--- mobile/api.js
+++ mobile/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://192.168.10.10:5000/api';
+const BASE_URL = 'http://192.168.10.100:5000/api';
 
 export const API = axios.create({ baseURL: BASE_URL });
